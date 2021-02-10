056a44ff2700334e73c74ff4a5fb6625d9d506cf
Joe Downard <joehdownard@gmail.com>
2021-02-10 18:51:45 +0000

    Added some basic styling for bill items.

--- Pages/Bills.js
+++ Pages/Bills.js
@@ -5,7 +5,7 @@ import {styles} from './Stylesheets/BillsStyles.js';
 
 export default function Bills() {
 
-    const [billData, setBillData] = useState([{name: 'Bill 1 Name', billDescription: 'Bill 1 Description'}, {name: 'Bill 2 Name', billDescription: 'Bill 2 Description'}]);
+    const [billData, setBillData] = useState([{name: 'Bill 1 Name', date: '12/02/2019', billDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut'}, {name: 'Bill 2 Name', date: '26/02/2020', billDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut'}]);
 
     if (!billData) {
         fetch("localhost:8080/get_bill/?<bill_id>&<meta_info>")
@@ -38,7 +38,7 @@ export default function Bills() {
 
 function BillList (props) {
     const billItemList = props.data.map((item) =>
-        <BillItem name={item.name} billDescription={item.billDescription}/>
+        <BillItem name={item.name} date={item.date} billDescription={item.billDescription}/>
     );
 
     return (
@@ -51,8 +51,12 @@ function BillList (props) {
 function BillItem (props) {
     return (
         <View style={styles.billContainer}>
-            <Text style={styles.billTitle}> {props.name} </Text>
-            <Text style={styles.billDescription}> {props.billDescription} </Text>
+            <View style={styles.billHeader}>
+                <Text style={styles.billTitle}> {props.name} </Text>
+                <Text style={styles.billHeaderRightSection}>{props.date}</Text>
+            </View>
+            <View style={styles.horizontalLine}/>
+            <Text style={styles.billDescription} includeFontPadding={false}> {props.billDescription} </Text>
         </View>
     )
 }
--- Pages/Stylesheets/BillsStyles.js
+++ Pages/Stylesheets/BillsStyles.js
@@ -3,7 +3,20 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     billContainer : {
         marginLeft: '5%',
+        marginRight: '5%',
         marginTop: '3%',
+        padding: 5,
+        backgroundColor: '#C4C4C4',
+    },
+
+    billHeader: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    billHeaderRightSection: {
+        margin: 5,
     },
 
     billTitle: {
@@ -12,7 +25,17 @@ export const styles = StyleSheet.create({
         fontWeight: 900,
     },
 
+    horizontalLine: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        marginLeft: 5,
+        marginRight: 5,
+    },
+
     billDescription: {
+        marginTop: 10,
+        marginLeft: 6,
+        textIndent: -4,
         fontStyle: 'Garamond',
         fontSize: 14,
     }
