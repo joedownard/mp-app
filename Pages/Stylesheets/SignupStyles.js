8b80717414353103686fab59ae1ddc394babdbce
Joe Downard <joehdownard@gmail.com>
2021-02-20 15:48:49 +0000

    Created signup page

--- Pages/Stylesheets/SignupStyles.js
+++ Pages/Stylesheets/SignupStyles.js
@@ -2,4 +2,30 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
 
+    signUpContainer: {
+
+    },
+
+    textInput: {
+        textAlign: 'center',
+        marginLeft: 25,
+        marginRight: 25,
+        marginTop: 20,
+        borderWidth: 2,
+        height: 30,
+        borderColor: '#C4C4C4',
+    },
+
+    signupButton: {
+        marginLeft: 50,
+        marginRight: 50,
+        marginTop: 25,
+    },
+
+    loginButton: {
+        marginLeft: 150,
+        marginRight: 150,
+        marginTop: 25,
+    },
+
 });
